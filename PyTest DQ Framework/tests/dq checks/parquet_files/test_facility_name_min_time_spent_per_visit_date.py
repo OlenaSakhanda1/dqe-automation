930e221c2663486ce5c1b6f78263db2c5b963604
OlenaSakhanda1 <130360301+OlenaSakhanda1@users.noreply.github.com>
2025-11-20 15:25:27 +0200

    Update test_facility_name_min_time_spent_per_visit_date.py

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -1,7 +1,11 @@
 """
-Description: Data Quality checks ...
+Description: Data Quality checks for migration from PostgreSQL to parquet
 Requirement(s): TICKET-1234
-Author(s): Name Surname
+Author(s): Olena Sakhanda
 """
 
 import pytest
+
+@pytest.mark.parquet_data
+def test_check_dataset_is_not_empty(target_data, data_quality_library):
+    data_quality_library.check_dataset_is_not_empty(target_data)
