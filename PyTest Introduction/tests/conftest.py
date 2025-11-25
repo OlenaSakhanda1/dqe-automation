70c3de5d6a97867aeab53ae4a779edc88fd844fa
OlenaSakhanda1 <130360301+OlenaSakhanda1@users.noreply.github.com>
2025-11-25 16:34:34 +0200

    Update conftest.py

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -16,10 +16,11 @@ def df(file_path):
 
 # Fixture to validate the schema of the file
 @pytest.fixture(scope="session")
-def test_validate_schema(df, validate_schema):
-    expected_schema = ['id', 'name', 'age', 'email', 'is_active']
-    actual_schema = df.columns.tolist()
-    assert validate_schema(actual_schema, expected_schema), f"Schema does not match! Expected {expected_schema}, got {actual_schema}"
+def validate_schema(df):
+    def _validate(actual_schema, expected_schema):
+        return actual_schema == expected_schema
+    return _validate
+
 
 # Pytest hook to mark unmarked tests with a custom mark
 def pytest_collection_modifyitems(items):
